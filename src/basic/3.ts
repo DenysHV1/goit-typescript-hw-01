ff6fcea7d22938d4f0615876aaa03875e64f11ba
DenVik <denyagarkusha2@gmail.com>
2024-12-16 10:06:13 +0100

    add

---
+++ src/basic/3.ts
@@ -0,0 +1,5 @@
+type Mix = string | number;
+type Modal = "enable" | "disable";
+
+const value: Mix = "20";
+const isOpen: Modal = "disable";
---
+++ src/generics/5.ts
@@ -0,0 +1,12 @@
+export enum UserRole {
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
+}
+type RecordRole = Record<UserRole, string>;
+
+const RoleDescription: RecordRole = {
+  admin: "Admin User",
+  editor: "Editor User",
+  guest: "Guest User",
+};
